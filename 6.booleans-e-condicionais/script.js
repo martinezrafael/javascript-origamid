35bba482dd159f0867f28dcb096b477160c9a9cb
Rafael Molina Martinez <rafaelmartinez.contato@gmail.com>
2023-01-11 15:49:16 -0300

    aula boolean e condicionais

--- 6.booleans-e-condicionais/script.js
+++ 6.booleans-e-condicionais/script.js
@@ -106,3 +106,136 @@ console.log(10 === "10"); //false
 console.log(10 === 10); //true
 console.log(10 != "10"); //false
 console.log(10 !== "10"); //true
+
+//Operadores Lógicos &&
+
+//&& Compara se uma expressão e a outra é verdadeira
+
+console.log(true && true); //true
+console.log(true && false); ///false
+console.log(false && true); //false
+console.log("Gato" && "Cão"); //'Cão
+console.log(5 - 5 && 5 + 5); //0
+console.log("Gato" && false); //false
+console.log(5 >= 5 && 3 < 6); //true
+
+/*
+  Se ambos os valores forem true
+  ele irá retornar o último valor verificado.
+  Se algum valor for false ele irá retornar
+  o mesmo e não irá continuar a verificar os próximos.
+*/
+
+//Operadores Lógicos || (ou)
+//Compara se uma expressão ou outra é verdadeira
+
+console.log(true || true); //true
+console.log(true || false); //true
+console.log(false || true); //true
+console.log("Gato" || "Cão"); //'Gato'
+console.log(5 - 5 || 5 + 5); //10
+console.log("Gato" || false); //Gato
+console.log(5 >= 5 && 3 < 6); //true
+
+//Retorna o primeiro valor true que encontrar
+
+//Switch
+/*
+  Com o switch você pode verificar se uma variável é igual
+  a diferentes valores utilizando o case. Caso ela seja igual, você pode cancelar c ontinuação. O valor de default ocorrerá caso nenhuma das anteriores seja verdadeira.
+*/
+
+var corFavorita = "Azul";
+
+switch (corFavorita) {
+  case "Azul":
+    console.log("Olhe para o céu.");
+    break;
+  case "Vermelho":
+    console.log("Olhe para as rosas");
+    break;
+  case "Amarelo":
+    console.log("Olhe para o sol.");
+    break;
+  default:
+    console.log("Feche os olhos");
+}
+
+//Exercício
+
+/*
+Verifique se a sua idade é maior que a de algum parente
+Dependendo do resultado coloque no console 'É maior', ´É igual' ou 'É menor'
+*/
+
+var minhaIdade = 31;
+var idadeParente = 73;
+
+if (minhaIdade > idadeParente) {
+  console.log("E maior");
+} else if (minhaIdade === idadeParente) {
+  console.log("É igual");
+} else {
+  console.log("É menor");
+}
+
+//Qual é o valor rtornado na seguinte expressão?
+var expressao = 5 - 2 && 5 - " " && 5 - 2; //3
+console.log(expressao);
+
+//Varifique se as seguiintes varuáveis são Truthy ou Falsy
+var nome = "André"; //true;
+var idade = 28; //true
+var possuiDoutorado = false; //false
+var empregoFuturo; //false
+var dinheiroNaConta = 0; //false
+
+if (nome) {
+  console.log("Verdadeiro"); //true
+} else {
+  console.log("Falso");
+}
+
+if (idade) {
+  console.log("Verdadeiro"); //true
+} else {
+  console.log("Falso");
+}
+
+if (possuiDoutorado) {
+  console.log("Verdadeiro"); //false
+} else {
+  console.log("Falso");
+}
+
+if (empregoFuturo) {
+  console.log("Verdadeiro"); //false
+} else {
+  console.log("Falso");
+}
+
+if (dinheiroNaConta) {
+  console.log("Verdadeiro"); //false
+} else {
+  console.log("Falso");
+}
+
+//Compare o total de habitantes no Brasil com china (valor em milhões)
+
+var brasil = 207e9;
+var china = 1340e9;
+
+//O que irá aparecer no console?
+if ("Gato" === "gato" && 5 > 2) {
+  console.log("Verdadeiro"); //Falso
+} else {
+  console.log("Falso");
+}
+
+//O que irá aparecer no console?
+
+if ("Gato" === "gato" || 5 > 2) {
+  console.log("Gato" && "Cão"); //Cão
+} else {
+  console.log("Falso");
+}
