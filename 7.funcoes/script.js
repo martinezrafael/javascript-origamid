66eada508c9fbd67c9c2b9bbe57d40111be67e75
Rafael Molina Martinez <rafaelmartinez.contato@gmail.com>
2023-01-13 16:23:32 -0300

    segunda aula de funcoes

--- 7.funcoes/script.js
+++ 7.funcoes/script.js
@@ -85,3 +85,94 @@ addEventListener("click", function () {
   definido, escritas como
   function(){} ou () => {}
 */
+
+//Pode ou não retornar um valor
+/*
+  Quando não definimos o return, ela irá retornar undefined.
+  O código interno da função é executado normalmente,
+  independente de existir valor no return ou não.
+*/
+
+function imc2(peso, altura) {
+  const imc = peso / altura ** 2;
+  console.log(imc);
+}
+
+imc2(80, 1.8); //retorna o imc
+console.log(imc2(80, 1.8)); //retorna undefined
+
+//Valores retornados
+/*
+  Uma função pode retornar qualquer tipo de dado
+  e até outras funções
+*/
+
+function terceiraIdade(idade) {
+  if (typeof idade !== "number") {
+    return "Por favor preencha um número";
+  } else if (idade >= 60) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/* 
+  Cuidado, retornar difertentes 
+  tipos de dados na mesma função
+  não é uma boa ideia.
+*/
+
+console.log(terceiraIdade(100));
+
+//Escopo
+/* 
+  Variáveis e funções definidas dentro de um bloco {},
+  não são visíveis fora dele.
+*/
+
+function precisoVisitar(paisesVisitados) {
+  var totalPaises = 193;
+  return `Ainda faltam visitar ${totalPaises - paisesVisitados} países`;
+}
+
+console.log(precisoVisitar(34)); //Ainda faltam visitar 159 países
+
+// console.log(totalPaises); //totalPaises is not defined
+
+//Escopo Léxico
+/*
+  Funções conseguem acessar variáveis que foram criadas no contexto pai
+*/
+
+var profissao = "Designer";
+
+function dados() {
+  var nome = "André";
+  var idade = 28;
+
+  function outrosDados() {
+    var endereco = "Rio de Janeiro";
+    var idade = 29;
+    return `${nome}, ${idade}, ${endereco} ${profissao}`;
+  }
+
+  return outrosDados();
+}
+
+console.log(dados()); //Retorna 'André, 29, Rio de Janeiro, Designer'
+
+// console.log(outrosDados()); //Retorna um erro
+
+//Hoisting
+/*
+  Antes de executar uma função, o JS 'move' todas
+  as funções declaradas para a memória.
+*/
+
+imc3(80, 1.8); //imc aparece no console
+
+function imc3(peso, altura) {
+  const imc = peso / altura ** 2;
+  console.log(imc);
+}
