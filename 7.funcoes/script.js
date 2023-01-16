9a81b5d9ab5d1c062ab0020c83d3f26b071fc7d6
Rafael Molina Martinez <rafaelmartinez.contato@gmail.com>
2023-01-16 16:33:50 -0300

    exercicios aula de funcoes

--- 7.funcoes/script.js
+++ 7.funcoes/script.js
@@ -176,3 +176,72 @@ function imc3(peso, altura) {
   const imc = peso / altura ** 2;
   console.log(imc);
 }
+
+//Exercicios
+
+//Crie uma função para verificar se um valor é thruthy
+
+function isThruthy(valor) {
+  return !!valor;
+}
+
+console.log(isThruthy(0));
+
+//crie uma função matemática que retorne o perímetro de um qudrado
+//lembrando? perimetro é a soma dos quatro lados do quadrado
+
+function perimetroQuadrado(lado) {
+  return lado * 4;
+}
+
+console.log(perimetroQuadrado(3));
+
+//crie uma função que retorne o seu nome completo
+//ela deve possuir os parâmetros: nome e sobrenome
+function nomeCompleto(nome, sobrenome) {
+  console.log(`${nome} ${sobrenome}`);
+}
+
+console.log(nomeCompleto("Rafael", "Martinez"));
+
+//Crie uma função que verifica se um número é par
+function ePar(numero) {
+  if (numero % 2 === 0) {
+    return `${numero} é par`;
+  } else {
+    return `${numero} é impar`;
+  }
+}
+
+console.log(ePar(4));
+
+//Crie uma função que retorne o tipo de
+//dado do argumento passado nela (typeof)
+
+function typeData(data) {
+  return typeof data;
+}
+
+console.log(typeData(true));
+
+/*
+  addEventListener é uma função nativa do Javascript
+  o primeiro parâmetro é o evento que ocorre e o segundo
+  o callback, utilize essa função para mostrar no console o seu nome completo quando o evento 'scroll' ocorrer
+*/
+
+addEventListener("scroll", function () {
+  console.log("Rafael Molina Martinez");
+});
+
+//corrija o erro abaixo
+var totalPaises = 193;
+
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+function jaVisitei(paisesVisitados) {
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+precisoVisitar(20);
+jaVisitei(20);
